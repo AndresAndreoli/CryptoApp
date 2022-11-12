2c902f95121eb21b5be0173f4aa0b56571d3b39d
Andres Andreoli <andresandreolii@gmail.com>
2022-11-12 15:25:20 -0300

    layout done

---
+++ app/src/main/java/com/example/cryptoapp/view/splashActivity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptoapp.view.splashActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptoapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
