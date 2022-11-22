3c048ed8bb088cb24ec900d791e742ea2aceefe4
Andres Andreoli <andresandreolii@gmail.com>
2022-11-22 15:45:09 -0300

    navigation file and homefragment added

---
+++ app/src/main/java/com/example/cryptoapp/view/mainActivity/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.example.cryptoapp.view.mainActivity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cryptoapp.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
