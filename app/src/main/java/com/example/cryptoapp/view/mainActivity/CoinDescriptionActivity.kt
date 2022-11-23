6b1b51cac4e86b3927df28f98295b097c8b37cd5
Andres Andreoli <andresandreolii@gmail.com>
2022-11-23 07:58:49 -0300

    coin description layout created

---
+++ app/src/main/java/com/example/cryptoapp/view/mainActivity/CoinDescriptionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptoapp.view.mainActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptoapp.R
+
+class CoinDescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_description)
+    }
+}
